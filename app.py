3cfcfea222a7340b34b1627ecc808b61ad6620c6
SanteriVtj <santeri.vaataja@gmail.com>
2020-08-13 14:45:54 +0300

    select doesnt work

--- app.py
+++ app.py
@@ -1,17 +1,29 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template
 import os
+from flask.wrappers import Request
 from money_mul import *
+import numpy as np
+import pandas as pd
 
 app = Flask(__name__)
 port = int(os.environ.get("PORT", 5000))
 
 
 @app.route('/')
-def hello_world():
-    plot = graph_rr()
+def index():
+    feature = 'mm_rr'
+    plot = create_plot(feature)
     return render_template('index.html', plot=plot)
 
+@app.route('/graph', methods=['GET', 'POST'])
+def change_fetures():
+    return create_plot(Request.args['selected'])
+
+def create_plot(feature):
+    if feature == 'mm_rr':
+        return graph_rr()
+    elif feature == 'mm_c':
+        return graph_c()
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0',port=port)
