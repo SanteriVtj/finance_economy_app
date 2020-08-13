31b8d013de7ed9aefc96cbd9e6c44c4cf334904f
SanteriVtj <santeri.vaataja@gmail.com>
2020-08-13 16:03:37 +0300

    stil trying

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 import os
 from flask.wrappers import Request
 from money_mul import *
--- static/js/plots.js
+++ static/js/plots.js
@@ -1,16 +1,20 @@
-$('#first_cat').on('change',function(){
+$('select').change(function() {
+    alert($('#first_cat').val());
+});
 
+$('select').change(function() {
     $.ajax({
-        url: "/graph",
-        type: "GET",
-        contentType: 'application/json;charset=UTF-8',
-        data: {
-            'selected': document.getElementById('first_cat').value
-
-        },
-        dataType:"json",
-        success: function (data) {
-            Plotly.newPlot('bargraph', data );
-        }
-    });
-})
+            url: "/graph",
+            type: "GET",
+            contentType: 'application/json;charset=UTF-8',
+            data: {
+                'selected': document.getElementById('first_cat').value
+    
+            },
+            dataType:"json",
+            success: function (data) {
+                Plotly.newPlot(document.getElementById('graph'), data);
+            },
+            timeout: 500000
+        });
+});
